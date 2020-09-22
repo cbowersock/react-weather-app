03bb42b0ee0b6515720f07099a7a6f346525cfe1
Clint Bowersock <52680761+cbowersock@users.noreply.github.com>
2020-09-22 00:08:21 -0400

    added functionality to toggle units

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,14 +6,15 @@ const Form = () => {
     const [dataHasReturned, setDataHasReturned] = useState(false);
     const [userInput, setUserInput] = useState('');
     const [weatherData, setWeatherData] = useState({});
+    const [unit, setUnit] = useState('imperial');
 
 	const handleChange = event => {
         setUserInput(event.target.value);
     };
 
-	const handleClick = async (event) => {
+	const handleSubmit = async (event) => {
         event.preventDefault();
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&appid=1e5d6dea25bc9c6cfdd4725176d486f3`
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=${unit}&appid=1e5d6dea25bc9c6cfdd4725176d486f3`
         try {
             const res = await fetch(url);
             const data = await res.json();
@@ -24,17 +25,40 @@ const Form = () => {
         }
 		setDataHasReturned(true);
     };
+
+    const handleClick = event => {
+        const {id} = event.target;
+        setUnit(id);
+    }
     
 	return (
 		<>
-			{dataHasReturned === true ? <WeatherDisplay data={weatherData}/> : null}
-			<form onSubmit={handleClick}>
+			{dataHasReturned === true ? <WeatherDisplay data={weatherData} units={unit}/> : null}
+			<form onSubmit={handleSubmit}>
 				<input
 					type="text"
 					placeholder="city"
 					onChange={handleChange}
                     value={userInput}
 				></input>
+                <br/>
+                <label>Fahrenheit</label>
+                <input
+                    type="radio"
+                    id="imperial"
+                    name="units"
+                    onClick={handleClick}>
+                </input>
+                <br/>
+                <label>
+                    Celsius
+                </label>
+                <input
+                    type="radio"
+                    id="metric"
+                    name="units"
+                    onClick={handleClick}>
+                </input>
 				<button type="submit">Show me da weather!</button>
 			</form>
 		</>
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 const WeatherDisplay = (props) => {
 
-    const {data} = props;
+    const {data, units} = props;
 
     return (
         <>
         <h1>weather in {data.name}</h1>
-        <h2>it is now {data.main.temp} degrees F</h2>
+        <h2>it is now {data.main.temp} degrees {(units === 'imperial' ? 'F' : 'C')}</h2>
         </>
     )
 }
